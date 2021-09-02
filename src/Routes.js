c34364db71d414e8a80f802a3de1a6049f072dc8
caioliveira277 <caio.oliveira277@etec.sp.gov.br>
2021-09-02 20:59:20 -0300

    alterando rotas para modo hash

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import Login from "./pages/login";
 import Header from "./components/header";
 import Home from "./pages/home";
